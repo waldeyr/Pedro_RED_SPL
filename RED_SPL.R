ab3fc89ac6b62150db7bc2d56f50e342b948c326
waldeyr <waldeyr@gmail.com>
2021-12-03 10:36:35 -0300

    dataset manipulation;

--- RED_SPL.R
+++ RED_SPL.R
@@ -8,7 +8,10 @@ library(stringr)
 library(readr)
 
 # Reading data
-RED_SPL <- read_csv("~/GoogleDrive/UnB/projeto_pedro/RED_SPL.csv")
+RED_SPL <- read_csv("https://raw.githubusercontent.com/waldeyr/Pedro_RED_SPL/main/RED_SPL.csv")
 RED_SPL <- RED_SPL %>%
-  separate(subs, c("subs1", "subs2"), " ")
+  separate(col = subs, into = c("subs1", "subs2"), sep = " ")
+# Renaming columns
+names(RED_SPL) <- c("Serial", "Region", "Mutation_Position", "Freq_Edition_0", "Freq_Edition_1", "Nucleotide_Substitution_1", "Nucleotide_Substitution_2", "P_Value", "P_Value_Adjusted", "Sym", "A3SS", "A5SS", "MXE", "RI", "SE")
+# Overview of the dataset
 View(RED_SPL)
