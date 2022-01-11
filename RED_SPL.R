697aa1670bcc9a3aa110e0b92e9568c35c7f6773
waldeyr <waldeyr@gmail.com>
2022-01-11 10:36:37 -0300

    cleanning and formating the raw data table

--- RED_SPL.R
+++ RED_SPL.R
@@ -9,9 +9,10 @@ library(readr)
 
 # Reading data
 RED_SPL <- read_csv("https://raw.githubusercontent.com/waldeyr/Pedro_RED_SPL/main/RED_SPL.csv")
+
 RED_SPL <- RED_SPL %>%
   separate(col = subs, into = c("subs1", "subs2"), sep = " ")
-# Renaming columns
-names(RED_SPL) <- c("Serial", "Region", "Mutation_Position", "Freq_Edition_0", "Freq_Edition_1", "Nucleotide_Substitution_1", "Nucleotide_Substitution_2", "P_Value", "P_Value_Adjusted", "Sym", "A3SS", "A5SS", "MXE", "RI", "SE")
+names(RED_SPL) <- c("Serial", "Region", "Mutation_Position", "Freq_Edition_0", "Freq_Edition_1", "Nucleotide_Substitution_1", "Nucleotide_Substitution_2", "P_Value", "P_Value_Adjusted", "Sym", "Ensembl_Id", "Type", "A3SS", "A5SS", "MXE", "RI", "SE", "LogFcExp", "Delta_Free_Energy", "Genetic_Var", "Most_Severe_Cons", "All_Cons", "AA_Change", "Codons_Change")
 # Overview of the dataset
 View(RED_SPL)
+write.table(RED_SPL, file = "/home/waldeyr/GoogleDrive/UnB/projeto_pedro/Pedro_RED_SPL/RED_SPL.tsv", sep = "\t", row.names = TRUE)
